f1f0dcf10b344da51e5eb064ea7771dff7013b5a
Shawlycode <kutinaorlando@gmail.com>
2021-08-14 00:56:33 +0000

    setup component, componentDidMount,and api fetch methods

---
+++ src/UsersClass.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class UsersClass extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            users: [],
+        };
+    }
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((res) => res.json())
+            .then((users) => {
+                console.log("users:", users);
+                this.setState({ users: users })
+            })
+            .catch((err) => console.log(err))
+
+    }
+    render() {
+        return (
+            <div>
+                <h1>users</h1>
+                <hr />
+                <div>
+                   {this.state.users.map(user =>
+                   <div>
+                    <p>{user.name}</p>
+                    <p>{user.username}</p>
+                    <p>{user.email}</p>
+                    <hr />
+                    </div>)}
+                </div>
+            </div>
+        );
+
+    }
+}
+
+
+export default UsersClass;
+
