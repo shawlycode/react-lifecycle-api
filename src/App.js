2a1e590895b5c333fbeff5cd5d98739e4c232f44
Shawlycode <kutinaorlando@gmail.com>
2021-08-14 01:04:12 +0000

    imported usersFuncton component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import UsersFunction from './UsersFunction';
 // import UsersClass from './UsersClass';
 
 function App() {
   return (
     <div className="App">
    {/* <UsersClass /> */}
+   <UsersFunction />
     </div>
   );
 }
