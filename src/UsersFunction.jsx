3f314fedb9a740723dbd507be79e62bb70048fc3
Shawlycode <kutinaorlando@gmail.com>
2021-08-14 02:07:26 +0000

    setup axios request

--- src/UsersFunction.jsx
+++ src/UsersFunction.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function UsersFunction() {
+const UsersFunction = () => {
+    let myUsers = [];
+    const [users, setUsers] = useState(myUsers);
+
+    useEffect(() => {
+        axios
+            .get('https://jsonplaceholder.typicode.com/users')
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((err) => {
+                console.log(err));
+    });
     return (
         <div>
-            
+            <h1>Users</h1>
         </div>
-    )
-}
+    );
+};
 
-export default UsersFunction
+export default UsersFunction;
